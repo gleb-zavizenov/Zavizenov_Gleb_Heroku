952c7df64e3a72febf5c5ff03831f4228fe6504d
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2019-10-09 02:26:38 -0400

    Finished 2 sections for themobile version

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-console.log("JS check");
+const leftBtn = document.querySelector(".slider-controls-left");
+const rightBtn = document.querySelector(".slider-controls-right");
+const sliderSections =document.querySelector(".slider-sections");
+
+let index = 0;
+let right = 0;
+let sliderWidth = document.querySelector(".slider-container").offsetWidth;
+
+leftBtn.addEventListener("click", function(){
+    if(index > 0){
+        right = right - sliderWidth;
+        sliderSections.style.right = right + "px";
+        index--;
+    } else {
+        right = sliderWidth * 2;
+        sliderSections.style.right = right + "px";
+        index = 2;
+    }
+});
+
+rightBtn.addEventListener("click", function(){
+    if(index < 2){
+        right = right + sliderWidth;
+        sliderSections.style.right = right + "px";
+        index++;
+    }else{
+        rigth = 0;
+        index = 0;
+        sliderSections.style.right = 0 + "px";
+    }
+});
